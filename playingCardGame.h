ad72b33563e5db7b2b8673875a75cb103fee1b47
TheAjax <alexandergreenberg@hotmail.com>
2014-12-22 19:20:51 -0500

    Encapsulates game

---
+++ playingCardGame.h
@@ -0,0 +1,14 @@
+//
+//  playingCardGame.h
+//  CalcFlashDemo
+//
+//  Created by Alex Greenberg on 12/21/14.
+//  Copyright (c) 2014 MakerMason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "questionCard.h"
+
+@interface playingCardGame : NSObject
+
+@end
